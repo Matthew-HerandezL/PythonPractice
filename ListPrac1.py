00914a9f915fbf27fc3a59275d4e369815960b2e
Matthew-HerandezL <34288485+Matthew-HerandezL@users.noreply.github.com>
2018-03-20 17:33:06 -0500

    ListPrac1.py
    List Practice Work functions

---
+++ ListPrac1.py
@@ -0,0 +1,21 @@
+#Matthew H List prac.
+# You Do Round 3
+listy_list = ["raindrops", "roses", "kittens", "kettle", "comet"]
+del listy_list[3]
+print listy_list
+
+listy_list.append("mittens")
+print listy_list
+
+listy_list.remove("mittens")
+print listy_list
+
+
+
+# You Do Round 4
+
+last_list = [3, 1, 4]
+for x in last_list:
+  print x * 2
+#print last_list
+print last_list
